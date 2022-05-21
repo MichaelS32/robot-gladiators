f59a151d1de42fcc35f7a0393fb8ceabdd97ed24
MichaelsLaptop <michaelschneider3232@gmail.com>
2022-05-21 13:52:12 -0700

    initial game js

---
+++ assets/js/game.js
@@ -0,0 +1,11 @@
+var playerName = window.prompt("What is your robot's name?");
+window.alert(playerName);
+
+console.log(playerName + "is ready for battle!");
+
+// this creates a function named "fight"
+function fight() {
+    console.log("The fight has begun!");
+}
+
+// fight();
